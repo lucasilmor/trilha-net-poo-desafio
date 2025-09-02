f732d1ae21b948b7735f49862ddf51f97492c75e
lucasilmor <lucassimor2502@gmail.com>
2025-09-02 20:04:23 -0300

    desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone iphone = new Iphone(numero:"1190909090", modelo:"Iphone 8", imei:"OKOS123312", memoria:256);
+Smartphone nokia = new Nokia(numero: "11545435433", modelo: "Lumia 12", imei: "OKOS123dda2", memoria: 128);
+
+iphone.InstalarAplicativo("App store");
+iphone.Ligar();
+iphone.ReceberLigacao();
+
+nokia.InstalarAplicativo("Windows store");
+nokia.Ligar();
+nokia.ReceberLigacao();
